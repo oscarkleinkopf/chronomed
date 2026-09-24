f92c6432d2601fbf10d8a94916828354b70af564
Oscar Kleinkopf <oscar.kleinkopf@gmail.com>
2026-09-24 12:10:29 -0300

    fix(theme): add textSecondary color property to SeniorTheme

--- mobile/lib/core/theme/senior_theme.dart
+++ mobile/lib/core/theme/senior_theme.dart
@@ -1,9 +1,10 @@
-﻿import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class SeniorTheme {
   static const Color background = Color(0xFF0F172A);
   static const Color cardBackground = Color(0xFF1E293B);
   static const Color textPrimary = Color(0xFFF8FAFC);
+  static const Color textSecondary = Color(0xFF94A3B8);
   static const Color accentYellow = Color(0xFFFACC15);
   static const Color successGreen = Color(0xFF22C55E);
 
